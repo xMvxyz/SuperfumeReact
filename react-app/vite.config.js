ef1c1d067210834b09b29f094c5fda3c8d812d7a
xMvxyz <maximoagustinabarcasoto@gmail.com>
2025-12-01 01:18:22 -0300

    Fix vite config for dev and prod environments

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  // GitHub Pages base URL (nombre del repositorio)
-  base: '/SuperfumeReact/',
+  // GitHub Pages base URL (nombre del repositorio) - solo en producción
+  base: process.env.NODE_ENV === 'production' ? '/SuperfumeReact/' : '/',
   plugins: [react()],
   publicDir: '../assets',
   build: {
